f53f73e26258c90ae86e19e041dfeea36e4748c7
Kohei Suzuki <eagletmt@gmail.com>
2014-06-29 13:58:43 +0900

    Extract caption pid and PCR pid

--- assdumper/assdumper.go
+++ assdumper/assdumper.go
@@ -5,13 +5,16 @@ import "io"
 import "os"
 
 /*
-ISO: ISO/IEC 13818-1
+[B10]: ARIB-STD B10
+[ISO]: ISO/IEC 13818-1
 */
 
 const TS_PACKET_SIZE = 188
 
 type AnalyzerState struct {
-	pmtPids map[int]bool
+	pmtPids    map[int]bool
+	pcrPid     int
+	captionPid int
 }
 
 func main() {
@@ -31,6 +34,8 @@ func main() {
 
 	buf := make([]byte, TS_PACKET_SIZE)
 	state := new(AnalyzerState)
+	state.pcrPid = -1
+	state.captionPid = -1
 
 	for {
 		n, err := fin.Read(buf)
@@ -54,7 +59,7 @@ func assertSyncByte(packet []byte) {
 func analyzePacket(packet []byte, state *AnalyzerState) {
 	assertSyncByte(packet)
 
-	//payload_unit_start_indicator := (packet[1] & 0x40) != 0
+	payload_unit_start_indicator := (packet[1] & 0x40) != 0
 	pid := int(packet[1]&0x1f)<<8 | int(packet[2])
 	hasAdaptation := (packet[3] & 0x20) != 0
 	hasPayload := (packet[3] & 0x10) != 0
@@ -72,12 +77,23 @@ func analyzePacket(packet []byte, state *AnalyzerState) {
 				state.pmtPids = extractPmtPids(p[1:])
 				fmt.Fprintf(os.Stderr, "Found %d pids: %v\n", len(state.pmtPids), state.pmtPids)
 			}
+		} else if state.pmtPids != nil && state.pmtPids[pid] {
+			if state.captionPid == -1 && payload_unit_start_indicator {
+				// PMT section
+				pcrPid := extractPcrPid(p[1:])
+				captionPid := extractCaptionPid(p[1:])
+				if captionPid != -1 {
+					fmt.Fprintf(os.Stderr, "caption pid = %d, PCR_PID = %d\n", captionPid, pcrPid)
+					state.pcrPid = pcrPid
+					state.captionPid = captionPid
+				}
+			}
 		}
 	}
 }
 
 func extractPmtPids(payload []byte) map[int]bool {
-	// ISO 2.4.4.3
+	// [ISO] 2.4.4.3
 	// Table 2-25
 	table_id := payload[0]
 	pids := make(map[int]bool)
@@ -96,4 +112,49 @@ func extractPmtPids(payload []byte) map[int]bool {
 		index += 4
 	}
 	return pids
+}
+
+func extractPcrPid(payload []byte) int {
+	return (int(payload[8]&0x1f) << 8) | int(payload[9])
+}
+
+func extractCaptionPid(payload []byte) int {
+	// [ISO] 2.4.4.8 Program Map Table
+	// Table 2-28
+	table_id := payload[0]
+	if table_id != 0x02 {
+		return -1
+	}
+	section_length := int(payload[1]&0x0F)<<8 | int(payload[2])
+	if section_length >= len(payload) {
+		return -1
+	}
+
+	program_info_length := int(payload[10]&0x0F)<<8 | int(payload[11])
+	index := 12 + program_info_length
+
+	for index < 3+section_length-4 {
+		stream_type := payload[index+0]
+		ES_info_length := int(payload[index+3]&0xF)<<8 | int(payload[index+4])
+		if stream_type == 0x06 {
+			elementary_PID := int(payload[index+1]&0x1F)<<8 | int(payload[index+2])
+			subIndex := index + 5
+			for subIndex < index+ES_info_length {
+				// [ISO] 2.6 Program and program element descriptors
+				descriptor_tag := payload[subIndex+0]
+				descriptor_length := int(payload[subIndex+1])
+				if descriptor_tag == 0x52 {
+					// [B10] 6.2.16 Stream identifier descriptor
+					// 表 6-28
+					component_tag := payload[subIndex+2]
+					if component_tag == 0x87 {
+						return elementary_PID
+					}
+				}
+				subIndex += 2 + descriptor_length
+			}
+		}
+		index += 5 + ES_info_length
+	}
+	return -1
 }
