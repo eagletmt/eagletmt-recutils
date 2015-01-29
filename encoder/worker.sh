dc7157c5cea7a8d93768e3a49c542b07c58f0f45
Kohei Suzuki <eagletmt@gmail.com>
2015-01-29 13:06:28 +0900

    Change stop.txt path

--- encoder/worker.sh
+++ encoder/worker.sh
@@ -5,7 +5,7 @@ QUEUE=jobs
 TIMEOUT=0 # never
 PT_DIR=/home/pt
 
-while [ ! -r stop.txt ]
+while [ ! -r /tmp/stop-encode.txt ]
 do
   FNAME="$(redis-cli -h $REDIS_HOST -n $DB --raw BLPOP $QUEUE $TIMEOUT | tail -1)"
   echo "$FNAME"
