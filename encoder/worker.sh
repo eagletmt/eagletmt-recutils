6a28ac3b34a7486501e632122f0a36b253f10c05
Kohei Suzuki <eagletmt@gmail.com>
2015-01-29 13:07:15 +0900

    Consider redis disconnection

--- encoder/worker.sh
+++ encoder/worker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 REDIS_HOST=localhost
 DB=1
 QUEUE=jobs
@@ -8,6 +8,11 @@ PT_DIR=/home/pt
 while [ ! -r /tmp/stop-encode.txt ]
 do
   FNAME="$(redis-cli -h $REDIS_HOST -n $DB --raw BLPOP $QUEUE $TIMEOUT | tail -1)"
-  echo "$FNAME"
-  ./enc.rb "$PT_DIR/$FNAME".ts
+  if [ x"$FNAME" = x"" ]; then
+    # redis disconnected?
+    sleep 10
+  else
+    echo "$FNAME"
+    ./enc.rb "$PT_DIR/$FNAME".ts
+  fi
 done
